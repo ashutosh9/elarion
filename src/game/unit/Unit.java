1477343341e1b0e8e8dc5b138f1dfa731c4aa065
Jedy <jordangeorgiev92@gmail.com>
2010-03-17 14:04:54 +0200

    (no commit message)

--- src/game/unit/Unit.java
+++ src/game/unit/Unit.java
@@ -4,12 +4,13 @@ import game.main.Animation;
 import game.main.Sprite;
 
 public class Unit {
-
 	
 	// stats stuff effects spells level xp ranks hp mp skills etc
 	// stand animation images for heading left
 	// stand animation images for heading right
 	
-	
+	public Unit(){
+		
+	}
 	
 }
