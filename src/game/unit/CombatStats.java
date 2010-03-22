f6af3e6ea3f98666e9b9bb71ff06b6259436b079
ACER <ACER@localhost>
2010-03-22 08:35:33 +0200

    (no commit message)

--- src/game/unit/CombatStats.java
+++ src/game/unit/CombatStats.java
@@ -2,4 +2,47 @@ package game.unit;
 
 public class CombatStats {
 
+	private double strenght;
+	private double agility;
+	private double stamina;
+	private double intelligence;
+	private double health;
+	private double mana;
+	public void setStrenght(double strenght) {
+		this.strenght = strenght;
+	}
+	public double getStrenght() {
+		return strenght;
+	}
+	public void setAgility(double agility) {
+		this.agility = agility;
+	}
+	public double getAgility() {
+		return agility;
+	}
+	public void setStamina(double stamina) {
+		this.stamina = stamina;
+	}
+	public double getStamina() {
+		return stamina;
+	}
+	public void setIntelligence(double intelligence) {
+		this.intelligence = intelligence;
+	}
+	public double getIntelligence() {
+		return intelligence;
+	}
+	public void setHealth(double health) {
+		this.health = health;
+	}
+	public double getHealth() {
+		return health;
+	}
+	public void setMana(double mana) {
+		this.mana = mana;
+	}
+	public double getMana() {
+		return mana;
+	}
+	
 }
