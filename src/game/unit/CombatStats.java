d184f521cfd73669da511e9881f9c2c0df04a9eb
Jedy <jordangeorgiev92@gmail.com>
2010-03-23 13:29:42 +0200

    (no commit message)

--- src/game/unit/CombatStats.java
+++ src/game/unit/CombatStats.java
@@ -8,39 +8,51 @@ public class CombatStats {
 	private double intelligence;
 	private double health;
 	private double mana;
+	
 	public void setStrenght(double strenght) {
 		this.strenght = strenght;
 	}
+	
 	public double getStrenght() {
 		return strenght;
 	}
+	
 	public void setAgility(double agility) {
 		this.agility = agility;
 	}
+	
 	public double getAgility() {
 		return agility;
 	}
+	
 	public void setStamina(double stamina) {
 		this.stamina = stamina;
 	}
+	
 	public double getStamina() {
 		return stamina;
 	}
+	
 	public void setIntelligence(double intelligence) {
 		this.intelligence = intelligence;
 	}
+	
 	public double getIntelligence() {
 		return intelligence;
 	}
+	
 	public void setHealth(double health) {
 		this.health = health;
 	}
+	
 	public double getHealth() {
 		return health;
 	}
+	
 	public void setMana(double mana) {
 		this.mana = mana;
 	}
+	
 	public double getMana() {
 		return mana;
 	}
