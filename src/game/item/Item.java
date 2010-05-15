16b5622ad2a670f30d2c3d4ce87cd96a6bed4b30
Jedy <jordangeorgiev92@gmail.com>
2010-05-15 16:34:08 +0300

    (no commit message)

--- src/game/item/Item.java
+++ src/game/item/Item.java
@@ -1,5 +1,17 @@
 package game.item;
 
+import java.awt.Image;
+
 public class Item {
+
+	Image image;
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
 
 }
