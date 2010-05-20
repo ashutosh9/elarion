b7f6dab2b7e22f4d367385869ed32bff90867549
Jedy <Jedy@localhost>
2010-05-20 19:47:34 +0300

    (no commit message)

--- src/game/main/KeyMap.java
+++ src/game/main/KeyMap.java
@@ -11,112 +11,118 @@ public class KeyMap {
 		int keyCode = e.getKeyCode();
 		
 		int speed = 4;
-		
-		if(i==1){//pressed
-			
-			if(keyCode == KeyEvent.VK_ESCAPE){
-				mc.stop();
-			}
-			
-			if(keyCode == KeyEvent.VK_1){
-				mc.exit();
-			}
-			
-			if(keyCode == KeyEvent.VK_DOWN){
-				int x = mc.getCurrentPlayer().getCurrentView().getX();
-				int y = mc.getCurrentPlayer().getCurrentView().getY();
-				y+=mc.getScreenHeight()+2;
-				if(y<f.getHeight()){
-					if(f.getSquare(x, y)!=null || mc.getCurrentPlayer().getCurrentViewAbsY() != 0){
-						int temp = mc.getCurrentPlayer().getCurrentViewAbsY();
-						mc.getCurrentPlayer().setCurrentViewAbsY(temp+speed);
+		if(!mc.getCombatView().isCombat()){
+			if(i==1){//pressed
+				
+				if(keyCode == KeyEvent.VK_ESCAPE){
+					mc.stop();
+				}
+				
+				if(keyCode == KeyEvent.VK_1){
+					mc.exit();
+				}
+				
+				if(keyCode == KeyEvent.VK_DOWN){
+					int x = mc.getCurrentPlayer().getCurrentView().getX();
+					int y = mc.getCurrentPlayer().getCurrentView().getY();
+					y+=mc.getScreenHeight()+2;
+					if(y<f.getHeight()){
+						if(f.getSquare(x, y)!=null || mc.getCurrentPlayer().getCurrentViewAbsY() != 0){
+							int temp = mc.getCurrentPlayer().getCurrentViewAbsY();
+							mc.getCurrentPlayer().setCurrentViewAbsY(temp+speed);
+						}
 					}
 				}
-			}
-			
-			if(keyCode == KeyEvent.VK_UP){
-				int x = mc.getCurrentPlayer().getCurrentView().getX();
-				int y = mc.getCurrentPlayer().getCurrentView().getY();
-				y--;
-				if(y>=0){
-					if(f.getSquare(x, y)!=null || mc.getCurrentPlayer().getCurrentViewAbsY() != 0){
-						int temp = mc.getCurrentPlayer().getCurrentViewAbsY();
-						mc.getCurrentPlayer().setCurrentViewAbsY(temp-speed);
+				
+				if(keyCode == KeyEvent.VK_UP){
+					int x = mc.getCurrentPlayer().getCurrentView().getX();
+					int y = mc.getCurrentPlayer().getCurrentView().getY();
+					y--;
+					if(y>=0){
+						if(f.getSquare(x, y)!=null || mc.getCurrentPlayer().getCurrentViewAbsY() != 0){
+							int temp = mc.getCurrentPlayer().getCurrentViewAbsY();
+							mc.getCurrentPlayer().setCurrentViewAbsY(temp-speed);
+						}
 					}
 				}
-			}
+
 			
-			if(keyCode == KeyEvent.VK_LEFT){
-				int x = mc.getCurrentPlayer().getCurrentView().getX();
-				int y = mc.getCurrentPlayer().getCurrentView().getY();
-				x--;
-				if(x>=0){
-					if(f.getSquare(x, y)!=null || mc.getCurrentPlayer().getCurrentViewAbsX() != 0){
-						int temp = mc.getCurrentPlayer().getCurrentViewAbsX();
-						mc.getCurrentPlayer().setCurrentViewAbsX(temp-speed);
+				if(keyCode == KeyEvent.VK_LEFT){
+					int x = mc.getCurrentPlayer().getCurrentView().getX();
+					int y = mc.getCurrentPlayer().getCurrentView().getY();
+					x--;
+					if(x>=0){
+						if(f.getSquare(x, y)!=null || mc.getCurrentPlayer().getCurrentViewAbsX() != 0){
+							int temp = mc.getCurrentPlayer().getCurrentViewAbsX();
+							mc.getCurrentPlayer().setCurrentViewAbsX(temp-speed);
+						}
 					}
 				}
-			}
+					if(keyCode == KeyEvent.VK_RIGHT){
+						int x = mc.getCurrentPlayer().getCurrentView().getX();
+						int y = mc.getCurrentPlayer().getCurrentView().getY();
+						x+=mc.getScreenWidth()+2;
+						if(x<f.getWidth()){
+							if(f.getSquare(x, y)!=null || mc.getCurrentPlayer().getCurrentViewAbsX() != 0){
+								int temp = mc.getCurrentPlayer().getCurrentViewAbsX();
+								mc.getCurrentPlayer().setCurrentViewAbsX(temp+speed);
+							}
+						}
+					}
 			
-			if(keyCode == KeyEvent.VK_RIGHT){
-				int x = mc.getCurrentPlayer().getCurrentView().getX();
-				int y = mc.getCurrentPlayer().getCurrentView().getY();
-				x+=mc.getScreenWidth()+2;
-				if(x<f.getWidth()){
-					if(f.getSquare(x, y)!=null || mc.getCurrentPlayer().getCurrentViewAbsX() != 0){
-						int temp = mc.getCurrentPlayer().getCurrentViewAbsX();
-						mc.getCurrentPlayer().setCurrentViewAbsX(temp+speed);
+				
+				if(keyCode == KeyEvent.VK_S){
+					if(mc.getCurrentPlayer().getSelectedHero() != null) {
+						mc.getCurrentPlayer().getSelectedHero().moveOneSquare(2, f);
 					}
 				}
-			}
-			
-			if(keyCode == KeyEvent.VK_S){
-				if(mc.getCurrentPlayer().getSelectedHero() != null) {
-					mc.getCurrentPlayer().getSelectedHero().moveOneSquare(2, f);
+				
+				if(keyCode == KeyEvent.VK_W){
+					if(mc.getCurrentPlayer().getSelectedHero() != null) {
+						mc.getCurrentPlayer().getSelectedHero().moveOneSquare(1, f);
+					}
 				}
-			}
-			
-			if(keyCode == KeyEvent.VK_W){
-				if(mc.getCurrentPlayer().getSelectedHero() != null) {
-					mc.getCurrentPlayer().getSelectedHero().moveOneSquare(1, f);
+				
+				if(keyCode == KeyEvent.VK_A){
+					if(mc.getCurrentPlayer().getSelectedHero() != null) {
+						mc.getCurrentPlayer().getSelectedHero().moveOneSquare(3, f);
+					}
 				}
-			}
-			
-			if(keyCode == KeyEvent.VK_A){
-				if(mc.getCurrentPlayer().getSelectedHero() != null) {
-					mc.getCurrentPlayer().getSelectedHero().moveOneSquare(3, f);
+				
+				if(keyCode == KeyEvent.VK_D){
+					if(mc.getCurrentPlayer().getSelectedHero() != null) {
+						mc.getCurrentPlayer().getSelectedHero().moveOneSquare(4, f);
+					}
 				}
-			}
-			
-			if(keyCode == KeyEvent.VK_D){
-				if(mc.getCurrentPlayer().getSelectedHero() != null) {
-					mc.getCurrentPlayer().getSelectedHero().moveOneSquare(4, f);
+				
+				if(keyCode == KeyEvent.VK_Q){
+					if(mc.getCurrentPlayer().getSelectedHero() != null) {
+						mc.getCurrentPlayer().getSelectedHero().moveOneSquare(13, f);
+					}
 				}
-			}
-			
-			if(keyCode == KeyEvent.VK_Q){
-				if(mc.getCurrentPlayer().getSelectedHero() != null) {
-					mc.getCurrentPlayer().getSelectedHero().moveOneSquare(13, f);
+				
+				if(keyCode == KeyEvent.VK_E){
+					if(mc.getCurrentPlayer().getSelectedHero() != null) {
+						mc.getCurrentPlayer().getSelectedHero().moveOneSquare(14, f);
+					}
 				}
-			}
-			
-			if(keyCode == KeyEvent.VK_E){
-				if(mc.getCurrentPlayer().getSelectedHero() != null) {
-					mc.getCurrentPlayer().getSelectedHero().moveOneSquare(14, f);
+				
+				if(keyCode == KeyEvent.VK_Z){
+					if(mc.getCurrentPlayer().getSelectedHero() != null) {
+						mc.getCurrentPlayer().getSelectedHero().moveOneSquare(23, f);
+					}
 				}
-			}
-			
-			if(keyCode == KeyEvent.VK_Z){
-				if(mc.getCurrentPlayer().getSelectedHero() != null) {
-					mc.getCurrentPlayer().getSelectedHero().moveOneSquare(23, f);
+				
+				if(keyCode == KeyEvent.VK_C){
+					if(mc.getCurrentPlayer().getSelectedHero() != null) {
+						mc.getCurrentPlayer().getSelectedHero().moveOneSquare(24, f);
+					}
 				}
-			}
-			
-			if(keyCode == KeyEvent.VK_C){
-				if(mc.getCurrentPlayer().getSelectedHero() != null) {
-					mc.getCurrentPlayer().getSelectedHero().moveOneSquare(24, f);
+				
+				if(keyCode == KeyEvent.VK_B){
+					boolean b = mc.getCombatView().isCombat();
+					mc.getCombatView().setCombat(!b);
 				}
-			}
 			
 			else {
 				e.consume();
@@ -130,5 +136,30 @@ public class KeyMap {
 			e.consume();
 		}
 	}
-
+	else if(mc.getCombatView().isCombat()){
+			if(i==1){//pressed
+				
+				if(keyCode == KeyEvent.VK_ESCAPE){
+					mc.stop();
+				}
+				
+				if(keyCode == KeyEvent.VK_1){
+					mc.exit();
+				}
+				
+				if(keyCode == KeyEvent.VK_B){
+					boolean b = mc.getCombatView().isCombat();
+					mc.getCombatView().setCombat(!b);
+				}
+				
+			}
+			if(i==2) { //released
+			}
+			
+			if(i==3) { //typed
+				e.consume();
+			}
+		}
+	}
 }
+
