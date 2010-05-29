1c9d3ae4c71e52de639beba665378c3d8c389a6d
Lwerewolf <Lwerewolf@localhost>
2010-05-30 02:07:25 +0300

    CastleView Images excluding menu, almost no code

--- src/game/graphic/CastleView.java
+++ src/game/graphic/CastleView.java
@@ -1,12 +1,25 @@
 package game.graphic;
-
+/* IMAGES POSITIONING INFO
+ * Background - 1280x800, coords 0x0
+ * Town Hall - coords 504x0
+ * Barracks - coords 206x417
+ * Archery - coords 66x413
+ * MageTower - coords 627x296
+ * Market - coords 637x524
+ * Tavern - coords 865x508
+ * Keep - coords 855x208
+ */
+import java.util.ArrayList;
+import game.castle.Castle;
 import game.player.Player;
+import game.unit.Hero;
 
 public class CastleView {
 
 	private Player currentPlayer;
+	private Castle currentCastle;
 	
-	
-	
-	
+	public CastleView (Castle castle, Player player) {
+		
+	}
 }
