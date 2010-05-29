5c93be060ac214b57ce9717b4edf0b1ac053ead3
Lwerewolf <Lwerewolf@localhost>
2010-05-29 17:56:43 +0300

    CastleBuilding class pretty much finished, minor changes to Castle. Making the individual buildings now.

--- src/game/castle/Castle.java
+++ src/game/castle/Castle.java
@@ -2,14 +2,16 @@ package game.castle;
 
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.util.ArrayList;
 
 import game.field.Field;
 import game.field.Square;
 import game.main.Sprite;
 import game.player.Player;
+import game.unit.Hero;
 
 public class Castle {
-
+	private ArrayList<CastleBuilding> buildings = new ArrayList<CastleBuilding>();
 	private int x; 
 	private int y; 
 	private Player owner;
@@ -23,6 +25,7 @@ public class Castle {
 		x = 1;
 		y = 1;
 		selected = false;
+		
 	}
 	
 	public Player getOwner() {
--- src/game/castle/CastleBuilding.java
+++ src/game/castle/CastleBuilding.java
@@ -1,5 +1,51 @@
 package game.castle;
 
-public interface CastleBuilding {
+import game.main.Sprite;
 
+public class CastleBuilding {
+
+	private boolean built;
+	private int x;
+	private int y;
+	private int w;//width
+	private int h;//height
+	private Sprite currentSprite;
+	
+	public void build()	{
+		built = true;
+	}
+	
+	public void destroy() {
+			built = false;
+	}
+	
+	public boolean isBuilt() {
+		return built;
+	}
+
+	public void setLoc(int x, int y) {
+			this.x = x;
+			this.y = y;
+	}
+	
+	public int getX(){
+		return x;	
+	}
+	
+	public int getY(){
+		return y;
+	}
+
+	public void setSize(int w, int h) {
+		this.w = w;
+		this.h = h;
+	}
+	
+	public int getW() {
+		return w;
+	}
+	
+	public int getH() {
+		return h;
+	}
 }
