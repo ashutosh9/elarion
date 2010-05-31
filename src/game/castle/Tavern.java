f39f20b586a2437101d58e1afcfdf2bab2a40f7d
Lwerewolf <Lwerewolf@localhost>
2010-05-31 18:27:48 +0300

    CombatView background/buildings draw, now making gfx for the menu

--- src/game/castle/Tavern.java
+++ src/game/castle/Tavern.java
@@ -1,16 +1,16 @@
 package game.castle;
 
+import java.awt.Toolkit;
+
 public class Tavern extends CastleBuilding {
 
 	public Tavern(Castle owner) {
 		super(owner);
-		/* graphics information TODO
-		 * x =
-		 * y = 
-		 * w =
-		 * h =
-		 * sprite = 
-		 */
+		x = 865;
+		y = 508;
+		w = 320;
+		h = 208;
+		image = Toolkit.getDefaultToolkit().getImage("Images/castle/Tavern.png");
 		goldcost = 1000;
 		woodcost = 5;
 		stonecost = 5;
