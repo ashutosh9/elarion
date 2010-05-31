30b30ccbfc47862ecbdfd1ee3e2dbd2238141467
skodjabashev <skodjabashev@localhost>
2010-05-31 14:43:44 +0300

    (no commit message)

---
+++ src/game/battle/Combat.java
@@ -0,0 +1,18 @@
+package game.battle;
+
+import game.unit.Hero;
+import game.unit.Unit;
+
+public class Combat {
+	
+	public Hero attacker;
+	public Hero defender;
+
+	public Combat(Hero attacker , Hero defender){
+		
+		for(Unit u : attacker.getUnits()){
+		
+	}
+	
+	}
+}
