000a72aeb6df7c9874dddd8e9cd71214e5039fdc
Lwerewolf <Lwerewolf@localhost>
2010-06-06 00:00:35 +0300

    Entering castle works

--- src/game/field/SquareEvents.java
+++ src/game/field/SquareEvents.java
@@ -28,13 +28,9 @@ public class SquareEvents {
 	
 	public void enterCastle(Square s, Player p, MainClass mc){
 		if(s.getCastle()!=null) {
-			System.out.print("stage1 pass\n");
 			if (s.getCastle().getOwner() == p) {
-				System.out.print("stage2 pass\n");
-				System.out.print("stage3 pass\n");
-				if (mc != null) { System.out.print("MC OK\n"); } else { System.out.print("MC fail\n");}
+				mc.clearPath();
 				mc.enterCastle(s.getCastle());
-				System.out.print("stage4 pass, SquareEvent should be OK\n");
 			}
 		}
 	}
